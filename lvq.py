8597aa691ccedc3e47dac1ecb4a126a6e6e8ae4d
Rahmat Irfan <IrfanRahmat007@hotmail.com>
2014-12-16 01:24:27 +0700

    Versi baru

--- lvq.py
+++ lvq.py
@@ -9,7 +9,7 @@ dist_b=0.0
 dist_c=0.0
 a=0 #iter buat distance
 winner=0.0
-lrate=0.1#lrate
+lrate=0.05#lrate
 tau=0.3
 content = [[]]
 pilihan="Saya Ganteng"
@@ -25,17 +25,17 @@ kelas2=0
 
 #menyimpan semua data set di dalam array
 with open('normaldata.csv', 'r+') as csvfile:
-    read = csv.reader(csvfile, delimiter='\t')
-    for row in read:
-        content=list(read)
-        for x in range(len(content)):
-            for y in range(len(content[x])):
-                content[x][y] = float(content[x][y])
+	read = csv.reader(csvfile, delimiter='\t')
+	for row in read:
+		content=list(read)
+		for x in range(len(content)):
+			for y in range(len(content[x])):
+				content[x][y] = float(content[x][y])
 
 #inputkan baris 1 sebagai input pertama kali
 for x in range(1):
-    for y in range(len(content[x])):
-        input_lyr[y]=content[x][y]
+	for y in range(len(content[x])):
+		input_lyr[y]=content[x][y]
 
 #mulai lvq
 #salin dulu bobot nya dari data set yang ada
@@ -43,39 +43,44 @@ weight=list(content)
 weight[x][y]=float(weight[x][y])
 
 for x in range(len(content)):
-    #print weight node 1 dan 2
-    print 'iterasi ke '+str(x)
-    for b in range(7):
-        print weight[0][b]
-    print '\n'
-    for b in range(7):
-        print weight[1][b]
-    print '\n'
-    for b in range(7):
-        print weight[2][b]
-    print '\n'
-    #mulai lvq
-    #1 hitung euqlidian distance
-    for y in range(len(content[x])):
-        dist_a += (input_lyr[y] - weight[0][y]) ** 2
-        dist_b += (input_lyr[y] - weight[1][y]) ** 2
-        dist_c += (input_lyr[y] - weight[2][y]) ** 2
-    dist_a=sqrt(dist_a)
-    dist_b=sqrt(dist_b)
-    dist_c=sqrt(dist_c)
-    #2 tentukan winner
-    winner=min(dist_a,dist_b,dist_c)
-    #update bobot
-    if winner==dist_a:
-        for b in range(7):
-            weight[0][b] = weight[0][b] + lrate * (input_lyr[b]-weight[0][b])
-    elif winner==dist_b:
-        for b in range(7):
-            weight[0+1][b] = weight[0+1][b] + lrate * (input_lyr[b]-weight[0+1][b])
-    else:
-        for b in range(7):
-            weight[0+2][b] = weight[0+2][b] + lrate * (input_lyr[b]-weight[0+2][b])
-
+	#print weight node 1 dan 2
+	print 'iterasi ke '+str(x)
+	#mulai lvq
+	#1 hitung euqlidian distance
+	dist_a=0.0
+	dist_b=0.0
+	dist_c=0.0
+	for y in range(len(content[x])):
+		input_lyr[y]=content[x][y]
+	
+	for y in range(len(content[x])):
+		dist_a += (input_lyr[y] - weight[0][y]) ** 2
+		dist_b += (input_lyr[y] - weight[1][y]) ** 2
+		dist_c += (input_lyr[y] - weight[2][y]) ** 2
+	dist_a=sqrt(dist_a)
+	dist_b=sqrt(dist_b)
+	dist_c=sqrt(dist_c)
+	#2 tentukan winner
+	winner=min(dist_a,dist_b,dist_c)
+	#update bobot
+	if winner==dist_a:
+		for b in range(7):
+			weight[0][b] = weight[0][b] + lrate * (input_lyr[b]-weight[0][b])
+	elif winner==dist_b:
+		for b in range(7):
+			weight[1][b] = weight[1][b] + lrate * (input_lyr[b]-weight[1][b])
+	else:
+		for b in range(7):
+			weight[2][b] = weight[2][b] + lrate * (input_lyr[b]-weight[2][b])
+	for b in range(7):
+		print weight[0][b]
+	print '\n'
+	for b in range(7):
+		print weight[1][b]
+	print '\n'
+	for b in range(7):
+		print weight[2][b]
+	print '\n'
 #def save(): untuk menyimpan outrput LVQ ke file digunakan sebagai start weight pada RBFNN
 file = open("weight_log.txt", "w+")
 file.write('\r\n')
@@ -90,74 +95,76 @@ file.close()
 #Start RBFNN
 pilihan="n"
 while pilihan=="n":
-    pilihan=raw_input("This is end of LVQ method, wanna start RBFNN? (y/n)")
-    if pilihan=="y":
+	pilihan=raw_input("This is end of LVQ method, wanna start RBFNN? (y/n)")
+	if pilihan=="y":
 #masukkan weight awal RBFNN dari output weight nya LVQ
-        with open('weight_log.txt', 'r+') as csvfile:
-            read = csv.reader(csvfile, delimiter='\t')
-            for row in read:
-                rbf_weight=list(read)
-                for x in range(len(rbf_weight)):
-                    for y in range(len(rbf_weight[x])):
-                        rbf_weight[x][y] = float(rbf_weight[x][y])
+		with open('weight_log.txt', 'r+') as csvfile:
+			read = csv.reader(csvfile, delimiter='\t')
+			for row in read:
+				rbf_weight=list(read)
+				for x in range(len(rbf_weight)):
+					for y in range(len(rbf_weight[x])):
+						rbf_weight[x][y] = float(rbf_weight[x][y])
 for x in range(len(rbf_weight)):
-    for y in range(len(rbf_weight[x])):
-        print str(rbf_weight[x][y])
-    print '\n'
+	for y in range(len(rbf_weight[x])):
+		print str(rbf_weight[x][y])
+	print '\n'
 
 #inputkan baris 1 sebagai input pertama kali
-for x in range(1):
-    for y in range(len(content[x])):
-        input_lyr2[y]=content[x][y]
 #Mulai RBFNN
 for x in range(len(content)):
-    #print weight node 1 dan 2
-    print 'iterasi ke '+str(x)
-    for b in range(2):
-        print v[0][b]
-    print '\n'
-    for b in range(2):
-        print v[1][b]
-    print '\n'
-    for b in range(2):
-        print v[2][b]
-    print '\n'
-    #mulai RBFNN
-    #1 hitung euqlidian distance
-    for y in range(len(content[x])-1):
-        dist_a += (input_lyr2[y] - rbf_weight[0][y]) ** 2
-        dist_b += (input_lyr2[y] - rbf_weight[1][y]) ** 2
-        dist_c += (input_lyr2[y] - rbf_weight[2][y]) ** 2
-    dist_a=sqrt(dist_a)
-    dist_b=sqrt(dist_b)
-    dist_c=sqrt(dist_c)
-    #cari gaussian
-    gauss_a=exp(-((dist_a)**2)/2*(tau**2))
-    gauss_b=exp(-((dist_b)**2)/2*(tau**2))
-    gauss_c=exp(-((dist_c)**2)/2*(tau**2))
-    #cari output z
-    z1=(gauss_a*v[0][0])+(gauss_b*v[1][0])+(gauss_c*v[2][0])
-    z2=(gauss_a*v[0][1])+(gauss_b*v[1][1])+(gauss_c*v[2][1])
-    #update weight
-    #v_baru=v_lama+(l_rate*(kelas-output)*y)
+	for y in range(len(content[x])):
+		input_lyr2[y]=content[x][y]
+	dist_a=0.0
+	dist_b=0.0
+	dist_c=0.0
+	#print weight node 1 dan 2
+	print 'iterasi ke '+str(x)
+	for b in range(2):
+		print v[0][b]
+	print '\n'
+	for b in range(2):
+		print v[1][b]
+	print '\n'
+	for b in range(2):
+		print v[2][b]
+	print '\n'
+	#mulai RBFNN
+	#1 hitung euqlidian distance
+	for y in range(len(content[x])-1):
+		dist_a += (input_lyr2[y] - rbf_weight[0][y]) ** 2
+		dist_b += (input_lyr2[y] - rbf_weight[1][y]) ** 2
+		dist_c += (input_lyr2[y] - rbf_weight[2][y]) ** 2
+	dist_a=sqrt(dist_a)
+	dist_b=sqrt(dist_b)
+	dist_c=sqrt(dist_c)
+	#cari gaussian
+	gauss_a=exp(-((dist_a)**2)/2*(tau**2))
+	gauss_b=exp(-((dist_b)**2)/2*(tau**2))
+	gauss_c=exp(-((dist_c)**2)/2*(tau**2))
+	#cari output z
+	z1=(gauss_a*v[0][0])+(gauss_b*v[1][0])+(gauss_c*v[2][0])
+	z2=(gauss_a*v[0][1])+(gauss_b*v[1][1])+(gauss_c*v[2][1])
+	#update weight
+	#v_baru=v_lama+(l_rate*(kelas-output)*y)
 
-    #update weight
-    if content[x][7]==1:
-        kelas1=0
-        kelas2=1
-    elif content[x][7]==2:
-        kelas1=1
-        kelas2=0
-    else:
-        kelas1=1
-        kelas2=1
+	#update weight
+	if content[x][7]==1:
+		kelas1=0
+		kelas2=1
+	elif content[x][7]==2:
+		kelas1=1
+		kelas2=0
+	else:
+		kelas1=1
+		kelas2=1
 
-    v[0][0]=v[0][0]+(lrate*(kelas1-z1)*gauss_a)
-    v[1][0]=v[1][0]+(lrate*(kelas1-z1)*gauss_b)
-    v[2][0]=v[2][0]+(lrate*(kelas1-z1)*gauss_c)
-    v[0][1]=v[0][1]+(lrate*(kelas2-z2)*gauss_a)
-    v[1][1]=v[1][1]+(lrate*(kelas2-z2)*gauss_b)
-    v[2][1]=v[2][1]+(lrate*(kelas2-z2)*gauss_c)
+	v[0][0]=v[0][0]+(lrate*(kelas1-z1)*gauss_a)
+	v[1][0]=v[1][0]+(lrate*(kelas1-z1)*gauss_b)
+	v[2][0]=v[2][0]+(lrate*(kelas1-z1)*gauss_c)
+	v[0][1]=v[0][1]+(lrate*(kelas2-z2)*gauss_a)
+	v[1][1]=v[1][1]+(lrate*(kelas2-z2)*gauss_b)
+	v[2][1]=v[2][1]+(lrate*(kelas2-z2)*gauss_c)
 
 file = open("weight_log2.txt", "w+")
 file.write('\r\n')
@@ -166,43 +173,43 @@ file.close()
 
 print "weight_log >> output lvq, bobot w untuk ke hidden layer"
 with open('weight_log.txt', 'r+') as csvfile:
-    read = csv.reader(csvfile, delimiter='\t')
-    for row in read:
-        rbf_weight=list(read)
-        for x in range(len(rbf_weight)):
-            for y in range(len(rbf_weight[x])):
-                rbf_weight[x][y] = float(rbf_weight[x][y])
-                print rbf_weight[x][y]
-        print "\n"
+	read = csv.reader(csvfile, delimiter='\t')
+	for row in read:
+		rbf_weight=list(read)
+		for x in range(len(rbf_weight)):
+			for y in range(len(rbf_weight[x])):
+				rbf_weight[x][y] = float(rbf_weight[x][y])
+				print rbf_weight[x][y]
+		print "\n"
 
 print "weight_log2 >> bobot v untuk ke output layer"
 with open('weight_log2.txt', 'r+') as csvfile:
-    read = csv.reader(csvfile, delimiter='\t')
-    for row in read:
-        v=list(read)
-        for x in range(len(v)):
-            for y in range(len(v[x])):
-                v[x][y] = float(v[x][y])
-                print v[x][y]
-        print "\n"
+	read = csv.reader(csvfile, delimiter='\t')
+	for row in read:
+		v=list(read)
+		for x in range(len(v)):
+			for y in range(len(v[x])):
+				v[x][y] = float(v[x][y])
+				print v[x][y]
+		print "\n"
 '''
-        #pembulatan
-        if z1<0.5:
-                z1=0
-        else:
-                z1=1
-        if z2<0.5:
-                z2=0
-        else:
-                z2=1
+		#pembulatan
+		if z1<0.5:
+				z1=0
+		else:
+				z1=1
+		if z2<0.5:
+				z2=0
+		else:
+				z2=1
 
-        v_baru=v_lama+(l_rate*(kelas-output)*y)
-        if z1 < 0.5 and z2 >=0.5 :
-                kelas="1"
-        elif z1 >=0.5 and z2 <0.5:
-                kelas="2"
-        elif z1 >=0.5 and z2 >=0.5:
-                kelas="3"
-        else:
-                kelas="bukan biji"
+		v_baru=v_lama+(l_rate*(kelas-output)*y)
+		if z1 < 0.5 and z2 >=0.5 :
+				kelas="1"
+		elif z1 >=0.5 and z2 <0.5:
+				kelas="2"
+		elif z1 >=0.5 and z2 >=0.5:
+				kelas="3"
+		else:
+				kelas="bukan biji"
 '''
--- tes.py
+++ tes.py
@@ -1,4 +1,5 @@
 import csv
+import os
 from math import *
 
 input_lyr=[ 0.0 for i in range(8) ]
@@ -22,73 +23,86 @@ v=[]
 kelas1=0
 kelas2=0
 tes=[]
-min=[0.0 for i in range (7)]
-max=[0.0 for i in range (7)]
+min=[100.0 for i in range (7)]
+max=[-100.0 for i in range (7)]
 tengah=[0.0 for i in range (3)]
 out=[[0.0 for i in range (2)] for j in range (210)]
 
 #load weight_log dari input ke hidden
 with open('weight_log.txt', 'r+') as csvfile:
-        read = csv.reader(csvfile, delimiter='\t')
-        for row in read:
-                rbf_weight=list(read)
-                for x in range(len(rbf_weight)):
-                        for y in range(len(rbf_weight[x])):
-                                rbf_weight[x][y] = float(rbf_weight[x][y])
-                                print str(rbf_weight[x][y])+'\t'
-                        print '\n\n'
+	read = csv.reader(csvfile, delimiter='\t')
+	for row in read:
+		rbf_weight=list(read)
+		for x in range(len(rbf_weight)):
+			for y in range(len(rbf_weight[x])):
+				rbf_weight[x][y] = float(rbf_weight[x][y])
+				print str(rbf_weight[x][y])+'\t'
+			print '\n\n'
 
 
 
 #load weight_log dari hidden ke output
 with open('weight_log2.txt', 'r+') as csvfile:
-        read = csv.reader(csvfile, delimiter='\t')
-        for row in read:
-                v=list(read)
-                for x in range(len(v)):
-                        for y in range(len(v[x])):
-                                v[x][y] = float(v[x][y])
-                                print str(v[x][y])+'\t'
-                        print '\n'
+	read = csv.reader(csvfile, delimiter='\t')
+	for row in read:
+		v=list(read)
+		for x in range(len(v)):
+			for y in range(len(v[x])):
+				v[x][y] = float(v[x][y])
+				print str(v[x][y])+'\t'
+			print '\n'
 		
 
 #cari min max dari data set aslinya untuk normalisasi
-with open('normaldata.csv', 'r+') as csvfile:
-        read = csv.reader(csvfile, delimiter='\t')
-        for row in read:
-                content=list(read)
-                for x in range(len(content)):
-                        for y in range(len(content[x])):
-                                content[x][y] = float(content[x][y])
+with open('dataasli.txt', 'r+') as csvfile:
+	read = csv.reader(csvfile, delimiter='\t')
+	for row in read:
+		content=list(read)
+		for x in range(len(content)):
+			for y in range(len(content[x])):
+				content[x][y] = float(content[x][y])
 
 for x in range(len(content)):
-        for y in range(len(content[x])-1):
-                if content[x][y] > max[y]:
-                        max[y]=content[x][y]
+	for y in range(len(content[x])-1):
+		if content[x][y] > max[y]:
+			max[y]=content[x][y]
 
-                if content[x][y] < min[y]:
-                        min[y]=content[x][y]
+		if content[x][y] < min[y]:
+			min[y]=content[x][y]
+
+print "MIN"
+for x in range(len(min)):
+	print min[x]
+print '\n'
+
+print "MAX"
+for x in range(len(max)):
+	print max[x]
+print '\n'
+
 
 #load test case sekaligus normalisasi di array tes
 with open('tes.txt', 'r+') as csvfile:
-        read = csv.reader(csvfile, delimiter='\t')
-        for row in read:
-                tes=list(read)
-                for x in range(len(tes)):
-                        for y in range(len(tes[x])-1):
-                                tes[x][y] = float(tes[x][y])
-                                tes[x][y]=(tes[x][y]-min[y])/(max[y]-min[y])
-                                print tes[x][y]
-                        print '\n'
+	read = csv.reader(csvfile, delimiter='\t')
+	for row in read:
+		tes=list(read)
+		for x in range(len(tes)):
+			for y in range(len(tes[x])-1):
+				tes[x][y] = float(tes[x][y])
+				tes[x][y]=(tes[x][y]-min[y])/(max[y]-min[y])
+				print tes[x][y]
+			print '\n'
 r=0 #baris untuk iter weight_log2 panjangnya 2
 c=0 #kolom untuk iter weight_log2 panjangnya 3
 o=0 #iter output panjangnya 2
 out=[[0.0 for i in range (2)] for j in range (len(tes))]
 #feed foward untuk menentukan kelas dari data tes
 for x in range (len(tes)):
+	tengah=[0.0 for i in range (3)]
 	for i in range (3):
 		for y in range(len(tes[x])-1):
-			tengah[i]=tengah[i]+tes[x][y]*rbf_weight[x][y]
+			tengah[i]=tengah[i]+tes[x][y]*rbf_weight[i][y]
+
 		#aktivasi sigmoid
 		tengah[i]=1/(1+exp(-tengah[i]))
 		out[x][o]=tengah[i]*v[r][c]
@@ -101,17 +115,19 @@ for x in range (len(tes)):
 			r=0
 		if c==3:
 			c=0
+		print tengah[i]
+		#os.system("pause")
 
 for x in range (len(tes)):
-        print "Data ke-"+str(x)
-        for y in range (2):
-		print out[x][y]
-        print '\n'
+	print "Data ke-"+str(x)
 	for y in range (2):
-                if out[x][y]<0.5:
-                        out[x][y]=0
-                else:
-                        out[x][y]=1
 		print out[x][y]
-        print '\n'			
+	print '\n'
+	for y in range (2):
+		if out[x][y]<0.5:
+			out[x][y]=0
+		else:
+			out[x][y]=1
+		print out[x][y]
+	print '\n'			
 	#print tengah[x]
